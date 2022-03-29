bef93dd21524b21f32d6cd862eabc422462719ac
Martin Sapak <xsapak06@vutbr.cz>
2022-03-29 09:59:41 +0200

    Test even numbers

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -1,28 +1,17 @@
-def is_whole_number(number):
-    if number % 1 == 0:
-        return True
-    else:
-        return False
-
-def is_natural_nuber(number):
-    if number % 1 == 0 and number > 0:
-        return True
-    else:
-        return False
-
 def is_even_number(number):
-    if number % 2 == 0:
-        return True
-    else:
-        return False
+    list_of_results = []
+    for i in number:
+        if i % 2 == 0:
+            result = [i, True]
+            list_of_results.append(result)
+        else:
+            result = [i, False]
+            list_of_results.append(result)
+    return  list_of_results
 
 def main():
-    my_number = -2
-    result_whole = is_whole_number(my_number)
-    print(f'Is whole number: {result_whole}')
-    result_natural = is_natural_nuber(my_number)
-    print(f'Is natural number: {result_natural}')
-    result_even = is_even_number(my_number)
+    my_list_of_numbers = [1, 2, 3, 5]
+    result_even = is_even_number(my_list_of_numbers)
     print(f'Is even number: {result_even}')
 
 if __name__ == '__main__':
