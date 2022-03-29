93530103bb3599d745f4d3f181711d118d8fa18c
Martin Sapak <xsapak06@vutbr.cz>
2022-03-29 09:04:31 +0200

    Is whole number, Is natural number, Is even number

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -1,13 +1,29 @@
 def is_whole_number(number):
+    if number % 1 == 0:
+        return True
+    else:
+        return False
+
+def is_natural_nuber(number):
     if number % 1 == 0 and number > 0:
+        return True
+    else:
+        return False
+
+def is_even_number(number):
+    if number % 2 == 0:
         return True
     else:
         return False
 
 def main():
     my_number = -2
-    result = is_whole_number(my_number)
-    print(result)
+    result_whole = is_whole_number(my_number)
+    print(f'Is whole number: {result_whole}')
+    result_natural = is_natural_nuber(my_number)
+    print(f'Is natural number: {result_natural}')
+    result_even = is_even_number(my_number)
+    print(f'Is even number: {result_even}')
 
 if __name__ == '__main__':
     main()
